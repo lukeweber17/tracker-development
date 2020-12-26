523f6d2d1a78dd4402bfc6d74d1e4c8c347487fd
lukeweber17 <luke.weber@valpo.edu>
2020-12-26 16:00:53 -0600

    updated issues page

--- src/components/issues/CreateIssue.js
+++ src/components/issues/CreateIssue.js
@@ -1,16 +1,15 @@
 import React, { useState, useRef } from 'react'
-import { ToggleButton, Button, Modal, Form, ButtonGroup } from 'react-bootstrap'
+import { ToggleButton, Button, Modal, Form, ButtonGroup, Row, Col } from 'react-bootstrap'
 import "../../styles/issues/createissue.css"
-import { useAuth } from "../../contexts/AuthContext"
 import { db, auth } from '../../firebase';
 
 export default function CreateIssue() {
 
-    const summaryRef = useRef()
     const descriptionRef = useRef()
     const projectRef = useRef()
-    const [radioValue, setRadioValue] = React.useState("");
- 
+    const titleRef = useRef()
+    const [radioValue, setRadioValue] = React.useState("")
+
     const radios = [
         { name: "Fire", value: "fire" },
         { name: "Hot", value: "hot" },
@@ -20,11 +19,16 @@ export default function CreateIssue() {
     const id = auth.currentUser.uid
 
     function handleSubmit() {
-        db.ref("users/"+id+"/issues").push({
-            summary: summaryRef.current.value,
+        const date = new Date()
+        const fullDate = date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear()
+        const dateValue = fullDate
+
+        db.ref("users/" + id + "/issues").push({
+            title: titleRef.current.value,
             priority: radioValue,
             description: descriptionRef.current.value,
-            project: projectRef.current.value
+            project: projectRef.current.value,
+            date: dateValue
         });
     }
 
@@ -35,35 +39,46 @@ export default function CreateIssue() {
             </Modal.Header>
             <Modal.Body>
                 <Form onSubmit={handleSubmit}>
+
                     <Form.Group controlId="string">
-                        <Form.Label>Summary</Form.Label>
-                        <Form.Control ref={summaryRef} type="text" placeholder="Summary" />
-                    </Form.Group>
-                    <Form.Label>Priority</Form.Label>
-                    <br />
-                    <ButtonGroup toggle className="mb-2">
-                        {radios.map((radio, index) => (
-                            <ToggleButton
-                                key={index}
-                                type="radio"
-                                name="radio"
-                                value={radio.value}
-                                checked={radioValue === radio.value}
-                                onChange={e => setRadioValue(e.currentTarget.value)}
-                            >
-                                {radio.name}
-                            </ToggleButton>
-                        ))}
-                    </ButtonGroup>
-                    <Form.Group controlId="string">
-                        <Form.Label>Description</Form.Label>
-                        <Form.Control ref={descriptionRef} type="text" placeholder="Description" />
-                    </Form.Group>
-                    <Form.Group controlId="string">
-                        <Form.Label>Project</Form.Label>
-                        <Form.Control ref={projectRef} type="text" placeholder="Project" />
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control ref={titleRef} type="text" placeholder="Title" />
                     </Form.Group>
 
+
+                    <Row>
+                        <Col xs={6}>
+                            <Form.Group controlId="string">
+                                <Form.Label>Project</Form.Label>
+                                <Form.Control ref={projectRef} type="text" placeholder="Project" />
+                            </Form.Group>
+                        </Col>
+                        <Col xs={6}>
+                            <Form.Label>Priority</Form.Label>
+                            <br />
+                            <ButtonGroup toggle className="mb-2">
+                                {radios.map((radio, index) => (
+                                    <ToggleButton
+                                        key={index}
+                                        type="radio"
+                                        name="radio"
+                                        value={radio.value}
+                                        checked={radioValue === radio.value}
+                                        onChange={e => setRadioValue(e.currentTarget.value)}
+                                    >
+                                        {radio.name}
+                                    </ToggleButton>
+                                ))}
+                            </ButtonGroup>
+                        </Col>
+                    </Row>
+
+
+
+                    <Form.Group controlId="string">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control as="textarea" ref={descriptionRef} type="text" placeholder="Description" />
+                    </Form.Group>
                     <Modal.Footer>
                         <Button variant="primary" type="submit">
                             Create!
--- src/components/issues/IssuesContent.js
+++ src/components/issues/IssuesContent.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Card, Col, Row } from "react-bootstrap"
 import "../../styles/issues/issuescontent.css"
-import { db, auth} from '../../firebase'
+import { db, auth } from '../../firebase'
 import BootstrapTable from 'react-bootstrap-table-next'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -11,20 +11,22 @@ export default class IssuesContent extends React.Component {
 
         this.state = {
             issueslist: [],
+            currTitle: "",
             currSummary: "",
             currPriority: "",
             currDescription: "",
-            currProject: ""
+            currProject: "",
+            currDate: ""
         };
     }
-    
+
     componentDidMount() {
         this.getUserData()
     }
 
     getUserData = () => {
         const userID = auth.currentUser.uid
-        db.ref('users/'+ userID +'/issues').on("value", snapshot => {
+        db.ref('users/' + userID + '/issues').on("value", snapshot => {
             let issueslist = [];
             snapshot.forEach(snap => {
                 // snap.val() is the dictionary with all your keys/values from the 'issues' path
@@ -35,91 +37,98 @@ export default class IssuesContent extends React.Component {
     }
 
     render() {
-        
+
         const rowStyle = () => {
             return {
                 backgroundColor: '#f8f8f6',
-                fontSize: '14px'
+                fontSize: '14px',
+                height: '100%'
             };
         };
 
         const columns = [{
-            dataField: 'summary',
-            text: 'Summary',
-            headerStyle: { 
-                backgroundColor: '#c5cbe3',
-            }
-        }, {
-            dataField: 'priority',
-            text: 'Priority',
-            headerStyle: { 
-                backgroundColor: '#c5cbe3',
-            }
-        }, {
-            dataField: 'description',
-            text: 'Description',
-            headerStyle: { 
+            dataField: 'title',
+            text: 'Title',
+            headerStyle: {
                 backgroundColor: '#c5cbe3',
             }
         }, {
             dataField: 'project',
             text: 'Project',
-            headerStyle: { 
+            headerStyle: {
+                backgroundColor: '#c5cbe3',
+            }
+        }, {
+            dataField: 'priority',
+            text: 'Priority',
+            headerStyle: {
                 backgroundColor: '#c5cbe3',
             }
         }];
 
-
         const rowEvents = {
             onClick: (e, row) => {
-                this.setState({ currSummary: row.summary });
+                this.setState({ currTitle: row.title });
                 this.setState({ currPriority: row.priority });
                 this.setState({ currDescription: row.description });
                 this.setState({ currProject: row.project });
+                this.setState({ currDate: row.date })
             }
         };
 
+
         return (
             <Row className="wrapper">
-                <Col className="dbtable" xs={6}>
-                    <BootstrapTable
-                        keyField='id'
-                        data={this.state.issueslist}
-                        columns={columns}
-                        rowStyle={rowStyle}
-                        rowEvents={rowEvents}
-                    />
+                <Col xs={6}>
+                    <Card className="issues-list">
+                        <BootstrapTable
+                            keyField='id'
+                            data={this.state.issueslist}
+                            columns={columns}
+                            rowStyle={rowStyle}
+                            rowEvents={rowEvents}
+                        />
+                    </Card>
                 </Col>
                 <br />
                 <Col xs={6}>
-                    <Card >
-                        <Card.Header style={{backgroundColor: '#c5cbe3', color: '#212529'}}>Selected Issue</Card.Header>
-                        <Card.Body style={{backgroundColor: '#f8f8f6'}}>
-                            <Card.Title>Description</Card.Title>
-                            <Card.Text>
-                                {this.state.currDescription}
-                            </Card.Text>
-                            <br />
+                    <Card className="current-issue-card">
+                        <Card.Header style={{ backgroundColor: '#c5cbe3', color: '#212529', fontWeight: 'bold' }}>Selected Issue</Card.Header>
+                        <Card.Body style={{ backgroundColor: '#f8f8f6' }}>
                             <Row>
                                 <Col xs={6}>
-                                    <Card.Title>Project</Card.Title>
-                                    <Card.Text>
-                                        {this.state.currProject}
+                                    <Card.Title>Title</Card.Title>
+                                    <Card.Text className="card-content">
+                                        {this.state.currTitle}
                                     </Card.Text>
                                 </Col>
                                 <Col xs={6}>
                                     <Card.Title>Priority</Card.Title>
-                                    <Card.Text>
+                                    <Card.Text className="card-content">
                                         {this.state.currPriority}
                                     </Card.Text>
                                 </Col>
                             </Row>
-                            <br />
-                            <Card.Title>Summary</Card.Title>
-                            <Card.Text>
-                                {this.state.currSummary}
+                            <br/>
+                            <Row>
+                                <Col xs={6}>
+                                    <Card.Title>Project</Card.Title>
+                                    <Card.Text className="card-content">
+                                        {this.state.currProject}
+                                    </Card.Text>
+                                </Col>
+                                <Col xs={6}>
+                                    <Card.Title>Date Created</Card.Title>
+                                    <Card.Text className="card-content">
+                                        {this.state.currDate}
+                                    </Card.Text>
+                                </Col>
+                            </Row>
+                            <br/>
+                            <Card.Title>Description</Card.Title>
+                            <Card.Text className="card-content">
+                                {this.state.currDescription}
                             </Card.Text>
-
                         </Card.Body>
                     </Card>
                 </Col>
